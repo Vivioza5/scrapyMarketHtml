52069473571e0869e28939769a8bccf6aea97a8f
Vitaliy <scromnyagga@gmail.com>
2021-09-03 09:48:09 +0300

    clean main spider

--- marketscrapy.py
+++ marketscrapy.py
@@ -14,24 +14,7 @@ class MarketscrapySpider(scrapy.Spider):
 
 
     def parse(self, response):
-        description=[]
-        html_document =response.text
-        tree=html.fromstring(html_document)
-        body_tags = tree.xpath('//a[@class="description"]')
-        # descr=[]
-        page_html = BeautifulSoup(response.text, 'html.parser')
-        descript=page_html.find('a', class_="description")
-        # print(descript.get_text())
 
-
-        # for item in body_tags:
-        #     if item:
-        #         body = item
-        #         text_document = body.text_content().strip()
-        #         descr.append(text_document)
-        #
-        #     else:
-        #         text_document = ''
         for item_div in response.xpath('//article[@class="item td-table"]'):
                 link=item_div.xpath('./descendant::div[@class="catalog-block-head"]/a')
                 href=link.xpath('./@href').get()
@@ -41,15 +24,6 @@ class MarketscrapySpider(scrapy.Spider):
                 price = raw_price and clean_price(raw_price) or None
                 descr=item_div.xpath('./descendant::a[@class="description"]').get()
                 descript= re.sub('<[^<]+?>', '',descr)
-
-                # # descr=item_div.xpath('//article[@class="item td-table"]/descendant::a[@class="description"]').get()
-                #
-                #
-                # # print(str(descript).replace('<span class="separator">•</span>',' '))
-                # description=clean_descr(descr)
-                # print(description.get_text())
-
-
 
 
                 yield{
